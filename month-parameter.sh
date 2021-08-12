6d91833b86245e6130b7025f352ee1b6a43ccb8f
root <root@ip-172-31-9-177.ap-south-1.compute.internal>
2021-08-12 11:57:32 +0000

    Month parameter

---
+++ month-parameter.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+case $n in
+      
+        1)
+                echo "This month is January" ;;
+        2)
+                 echo "This month is Feburary" ;;
+        3)
+                 echo "This month is March" ;;
+        4)
+                 echo "This month is April" ;;
+        5)
+                  echo "This month is May" ;;
+        6)
+                 echo "This month is June" ;;
+        7)
+       		echo "This month is July" ;;
+            
+         8)
+        	  echo "This month is August" ;;
+         9)
+       		  echo "This month is Septmber" ;;
+         10)
+         	 echo "This month is October" ;;   
+         11)
+        	 echo "This month is November" ;;
+         12)
+          	 echo "This month is December" ;;
+         
+        
+            
+esac
+
