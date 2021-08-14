f72771aeb32b0017bb0532386e0d5ae264c87998
root <root@ip-172-31-9-177.ap-south-1.compute.internal>
2021-08-14 13:02:45 +0000

    Backup task

--- backup-script.sh
+++ backup-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DATE=$(date +%S-%M-%H-%d-%m-%Y)
-BACKUP_DIR=/var/log/backup
+BACKUP_DIR=/root/backup
 
-tar -czvf $BACKUP_DIR/shell-scripts-$DATE.tar.gz /var/log/jenkins
+tar -czvf $BACKUP_DIR/shell-scripts-$DATE.tar.gz /root/my-shell-scripts
